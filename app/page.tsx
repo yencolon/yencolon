f189bd425da1448167e7f83fab6a1da2f3150d4c
Yen Colon <yenjcc@gmail.com>
2024-10-27 20:30:59 -0400

    Update styles

--- app/page.tsx
+++ app/page.tsx
@@ -28,58 +28,55 @@ const socialLinks = [
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6 sm:p-24">
-      <div className="flex flex-col items-center flex-1 w-full">
-        <div className="mb-6">
+    <main className="flex min-h-screen flex-col items-center justify-center p-6 sm:p-12 bg-gradient-to-br from-blue-800 to-blue-900">
+      <div className="flex w-full max-w-4xl bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
+        <div className="flex flex-col items-start justify-center p-8 bg-gradient-to-br from-blue-700 to-blue-800 text-white rounded-tl-lg rounded-bl-lg shadow-inner">
           <Image
-            src={profileImageUrl}
-            alt="Profile Picture"
-            width={150}
-            height={150}
-            className="rounded-full"
-          />
+              src={profileImageUrl}
+              alt="Profile Picture"
+              width={150}
+              height={150}
+              className="rounded-full shadow-lg"
+            />
         </div>
-        <p className="text-4xl sm:text-7xl font-bold text-left first-letter:text-cyan-700 dark:text-white">
-          Yen Colon
-        </p>
-        <p className="text-base sm:text-lg font-light text-left pt-2">
-          Software developer
-        </p>
-        <div className="pt-4 w-full sm:w-3/6">
-          <p className="text-sm sm:text-base mb-3 text-gray-500 dark:text-gray-400 px-2 sm:px-0">
-            Software developer with over 4 years of experience in the field, possessing strong expertise in front-end and back-end software development. Started as a freelance developer, creating mobile apps and APIs for clients. Currently focused on Java, Kotlin and JavaScript languages.
+        <div className="flex flex-col items-center flex-1 p-8">
+          <p className="text-4xl sm:text-6xl font-bold text-left text-blue-300 dark:text-blue-200">
+            Yen Colon
           </p>
-        </div>
-        <div className="pt-4 w-full sm:w-3/6">
-          <div className="flex justify-around items-center mb-3">
-            {socialLinks.map(({ href, src, alt, label, username }) => (
-              <a
-                key={href}
-                href={href}
-                target="_blank"
-                rel="noreferrer"
-                aria-label={label}
-                className="flex flex-col items-center text-center w-24"
-              >
-                <div className="bg-white rounded-full p-2 shadow-md flex items-center justify-center w-16 h-16">
-                  <Image src={src} alt={alt} width={24} height={24} />
-                </div>
-                <span className="text-xs sm:text-sm text-gray-600 mt-2">{username}</span>
-              </a>
-            ))}
+          <p className="text-lg sm:text-xl font-light text-left pt-2 text-blue-200 dark:text-blue-100">
+            Software Developer
+          </p>
+          <div className="pt-4 w-full sm:w-4/5 lg:w-3/4">
+            <p className="text-sm sm:text-base mb-3 text-gray-300 dark:text-gray-500 text-justify leading-relaxed">
+              Software developer with over 4 years of experience in the field, possessing strong expertise in front-end and back-end software development. Started as a freelance developer, creating mobile apps and APIs for clients. Currently focused on Java, Kotlin, and JavaScript languages.
+            </p>
+          </div>
+          <div className="pt-4 w-full sm:w-3/4">
+            <div className="flex justify-evenly items-center">
+              {socialLinks.map(({ href, src, alt, label, username }) => (
+                <a
+                  key={href}
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label={label}
+                  className="flex flex-col items-center text-center w-24 hover:text-blue-300"
+                >
+                  <div className="bg-white rounded-full p-2 shadow-lg flex items-center justify-center w-16 h-16 mb-2 hover:bg-blue-200">
+                    <Image src={src} alt={alt} width={24} height={24} />
+                  </div>
+                  <span className="text-xs sm:text-sm text-gray-300 dark:text-gray-500">{username}</span>
+                </a>
+              ))}
+            </div>
           </div>
         </div>
-        <div>
-          <a href="https://app.daily.dev/yencolon"><img src="https://api.daily.dev/devcards/v2/N9KR9YPeTeM4HJDAPzGOI.png?type=default&r=zpv" width="356" alt="Yen Colon's Dev Card" /></a>
-        </div>
       </div>
-      <div className="flex flex-col flex-1 w-full">
-        <footer className="flex justify-end flex-col flex-1 w-full text-center mt-auto">
-          <p className="text-xs sm:text-sm text-gray-600">
-            &copy; {new Date().getFullYear()} Yen Colon. Todos los derechos reservados.
-          </p>
-        </footer>
-      </div>
+      <footer className="w-full text-center mt-8">
+        <p className="text-sm text-gray-300 dark:text-gray-500">
+          &copy; {new Date().getFullYear()} Yen Colon. Todos los derechos reservados.
+        </p>
+      </footer>
     </main>
   );
 }
