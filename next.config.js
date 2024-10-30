78a567a5682d40702e2188137f8234f824aa2e98
Yen Colon <yenjcc@gmail.com>
2024-10-30 13:35:48 -0400

    update security headers

--- next.config.js
+++ next.config.js
@@ -1,8 +1,42 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['github.com']
-    }
-}
+  images: {
+    domains: ["github.com"],
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self' data:; connect-src 'self' https://api.daily.dev https://api.github.com https://github.com https://www.linkedin.com; frame-src 'self' https://app.daily.dev; object-src 'none'; base-uri 'none'; form-action 'none'",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
