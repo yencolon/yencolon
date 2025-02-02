91e466878084d01d0f2b4e18b16da87067a0f0a6
Yen Colon <yenjcc@gmail.com>
2025-02-01 23:49:05 -0400

    Minimal ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { GamepadIcon, GithubIcon, LinkedinIcon, MailIcon } from "lucide-react";
+import {
+  GamepadIcon,
+  GithubIcon,
+  LinkedinIcon,
+  MailIcon,
+  CameraIcon,
+} from "lucide-react";
 import Image from "next/image";
 
 export default function Component() {
@@ -7,19 +15,20 @@ export default function Component() {
 
   const socialLinks = [
     {
-      href: "https://github.com/yencolon",
-      icon: GithubIcon,
-      alt: "GitHub",
-      label: "Visit GitHub profile",
-      openInNewTab: true,
-    },
-    {
       href: "https://www.linkedin.com/in/yencolon/",
       icon: LinkedinIcon,
       alt: "LinkedIn",
       label: "Visit LinkedIn profile",
       openInNewTab: true,
     },
+    {
+      href: "https://github.com/yencolon",
+      icon: GithubIcon,
+      alt: "GitHub",
+      label: "Visit GitHub profile",
+      openInNewTab: true,
+    },
+
     {
       href: "mailto:yenjcc@gmail.com",
       icon: MailIcon,
@@ -33,52 +42,58 @@ export default function Component() {
       alt: "Prove your geo knowledge",
       label: "Geo Play",
       openInNewTab: true,
-    }
+    },
+    {
+      href: "https://vsco.co/yencolon/",
+      icon: CameraIcon,
+      alt: "See the world through my lens",
+      label: "Photography",
+      openInNewTab: true,
+    },
   ];
 
   return (
-    <div className="relative min-h-screen bg-gradient-to-b from-black to-gray-800 p-8 flex flex-col items-center justify-center">
-      <div className="absolute inset-0 bg-gradient-to-r from-purple-900 via-indigo-900 to-black opacity-50"></div>
-      <Card className="relative w-full max-w-xl bg-black bg-opacity-40 text-white backdrop-blur-lg rounded-3xl shadow-2xl">
-        <CardContent className="p-12 flex flex-col gap-6 relative z-10">
-          <div className="flex justify-center mb-4">
-            <div className="relative w-48 h-48 rounded-full overflow-hidden ring-4 ring-indigo-500 shadow-lg">
-              <Image
-                src={profileImageUrl}
-                alt="Profile picture"
-                fill
-                className="object-cover"
-                priority
-              />
-            </div>
-          </div>
-          <div className="text-center space-y-4">
-            <h1 className="text-5xl font-extrabold text-transparent bg-gradient-to-r from-purple-400 to-pink-500 bg-clip-text">
-              Yen Colon
-            </h1>
-            <p className="text-gray-300">
-              Crafting software solutions with a passion for innovation and creativity.
-            </p>
-          </div>
-          <div className="flex justify-center gap-8 mt-8">
-            {socialLinks.map((link, index) => {
-              const Icon = link.icon;
-              return (
-                <a
-                  key={index}
-                  href={link.href}
-                  target={link.openInNewTab ? "_blank" : "_self"}
-                  rel={link.openInNewTab ? "noopener noreferrer" : ""}
-                  className="transition-transform transform hover:scale-110 bg-gradient-to-br from-indigo-600 to-purple-700 p-4 rounded-full flex items-center justify-center text-white shadow-inner hover:shadow-xl"
-                  aria-label={link.label}
-                >
-                  <Icon className="h-4 w-4 md:h-8 md:w-8" />
-                </a>
-              );
-            })}
-          </div>
-        </CardContent>
-      </Card>
+    <div className="flex justify-center items-center h-screen bg-slate-50 dark:bg-gray-900">
+      <div className="bg-orange-300 dark:bg-gray-800">
+        <div className="m-10">
+          <h2 className="text-4xl md:text-6xl font-bold font-mono text-black dark:text-white">
+            developer.
+          </h2>
+          <h2 className="text-4xl md:text-6xl font-bold font-mono text-black dark:text-white">
+            explorer.
+          </h2>
+          <h2 className="text-4xl md:text-6xl font-bold font-mono text-black dark:text-white">
+            photographer.
+          </h2>
+        </div>
+        <div className="m-10">
+          <p className="text-lg text-gray-800 dark:text-gray-200 font-mono typewriter flex-wrap">
+            Hi! My name is Yen Colon and a software developer who loves crafting
+            things.
+          </p>
+        </div>
+        <div className="m-10 flex flex-row space-x-8 ">
+          {socialLinks.map((link, index) => {
+            const Icon = link.icon;
+            return (
+              <a
+                key={index}
+                href={link.href}
+                target={link.openInNewTab ? "_blank" : "_self"}
+                rel={link.openInNewTab ? "noopener noreferrer" : ""}
+                aria-label={link.label}
+              >
+                <Icon className="text-black dark:text-white" />
+              </a>
+            );
+          })}
+        </div>
+        <div className="m-10">
+          <Button className="bg-black text-white dark:bg-white dark:text-black">
+            View my resume
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
