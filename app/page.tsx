21c5a459602b577e44fdf3de70a7c692a77b7319
Yen Colon <yenjcc@gmail.com>
2025-02-02 01:00:50 -0400

    Adding animation

--- app/page.tsx
+++ app/page.tsx
@@ -54,22 +54,25 @@ export default function Component() {
 
   return (
     <div className="flex justify-center items-center h-screen bg-slate-50 dark:bg-gray-900">
-      <div className="bg-orange-300 dark:bg-gray-800">
+      <div className="bg-orange-300 dark:bg-gray-800 border-2 border-neutral-800 dark:border-green-300 h-screen sm:h-auto">
+        <div className="bg-green-200 ">
+          <div>U+00A5 U+003A</div>
+        </div>
         <div className="m-10">
           <h2 className="text-4xl md:text-6xl font-bold font-mono text-black dark:text-white">
-            developer.
+            developer
           </h2>
           <h2 className="text-4xl md:text-6xl font-bold font-mono text-black dark:text-white">
-            explorer.
+            explorer
           </h2>
           <h2 className="text-4xl md:text-6xl font-bold font-mono text-black dark:text-white">
-            photographer.
+            photogr...
           </h2>
         </div>
         <div className="m-10">
           <p className="text-lg text-gray-800 dark:text-gray-200 font-mono typewriter flex-wrap">
-            Hi! My name is Yen Colon and a software developer who loves crafting
-            things.
+            Hi! My name is Yen Colon and I' a software developer who loves
+            crafting things.
           </p>
         </div>
         <div className="m-10 flex flex-row space-x-8 ">
@@ -89,9 +92,13 @@ export default function Component() {
           })}
         </div>
         <div className="m-10">
-          <Button className="bg-black text-white dark:bg-white dark:text-black">
+          <a
+            className="bg-black p-4 text-white dark:bg-white dark:text-black"
+            href="https://drive.google.com/file/d/1wzJ7rZCmVJn6o5y5V7t1Zk9X1V2b4wYg/view?usp=sharing"
+            target="_blank"
+          >
             View my resume
-          </Button>
+          </a>
         </div>
       </div>
     </div>
